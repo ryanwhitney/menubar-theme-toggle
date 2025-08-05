782ca6157ad2d0f72428bdc91aaa11b808f74ef9
Ryan Whitney <ryan.w@freedom.press>
2025-08-05 13:23:43 -0700

    Initial Commit

---
+++ menubar theme toggle/menubar_theme_toggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  menubar_theme_toggleApp.swift
+//  menubar theme toggle
+//
+//  Created by Ryan Whitney on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct menubar_theme_toggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
